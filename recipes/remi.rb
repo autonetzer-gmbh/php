6e316c7b7ecc7992d5155ffce83cf709953b0c3a
Felix Peters <felix@felixs-mbp.fritz.box>
2014-04-24 10:11:54 +0200

    add remi as install method

--- recipes/remi.rb
+++ recipes/remi.rb
@@ -1,6 +1,6 @@
 
-#yum_repository 'remi-php55' do
-#  description 'Les RPM de Remi PHP55 - Repository'
+yum_repository 'remi-php55' do
+  description 'Les RPM de Remi PHP55 - Repository'
   mirrorlist 'http://rpms.famillecollet.com/enterprise/6/php55/mirror'
   gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
   fastestmirror_enabled true
